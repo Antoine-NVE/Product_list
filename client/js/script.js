86cbe6b8e23ace0cfcf11d6ee2f83f2abb993897
Antoine-NVE <antoinenavette@gmail.com>
2024-05-02 14:02:15 +0200

    Affichage du formulaire d'ajout

--- client/js/script.js
+++ client/js/script.js
@@ -1,5 +1,6 @@
 const container = document.getElementById('container');
 
+// Fonction qui affiche la page d'accueil
 function showMain(products) {
     let body = '';
     for (let i = 0; i < products.length; i++) {
@@ -26,7 +27,7 @@ function showMain(products) {
             <tbody>
                 ${body}
                 <tr>
-                    <td colspan="4"><button id="btn-add" class="btn btn-primary d-block w-100">Ajouter un produit</button></td>
+                    <td colspan="4"><button id="btn-add" class="btn btn-primary w-100">Ajouter un produit</button></td>
                 </tr>
             </tbody
         </table>
@@ -34,7 +35,35 @@ function showMain(products) {
 
     const btnAdd = document.getElementById('btn-add');
     btnAdd.addEventListener('click', () => {
-        console.log('Ajout');
+        showAdd();
+    });
+}
+
+// Fonction qui affiche le formulaire d'ajout
+function showAdd() {
+    container.innerHTML = `
+        <button id="btn-main" class="btn btn-primary">Accueil</button>
+        <form>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    `;
+
+    const btnMain = document.getElementById('btn-main');
+    btnMain.addEventListener('click', () => {
+        showMain(products);
     });
 }
 
