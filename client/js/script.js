d7c010fcc3f402ccc689285cd4f5a9280fccf72f
Antoine-NVE <antoinenavette@gmail.com>
2024-05-05 01:49:36 +0200

    Déplacement des views

--- client/js/script.js
+++ client/js/script.js
@@ -1,67 +1,29 @@
 import Product from './models/Product.js';
-
-const container = document.getElementById('container');
+import { showMain, showCreate, showUpdate } from './views/product.js';
 
 function navigate() {
     const path = window.location.hash.split('/');
     path.shift();
 
     if (path[0] === undefined || path[0] === '') {
-        showMain();
+        main();
     } else if (path[0] === 'create') {
-        showCreate();
+        create();
     } else if (path[0] === 'update') {
-        showUpdate(path[1]);
+        update(path[1]);
     }
 }
 window.addEventListener('hashchange', navigate);
 window.addEventListener('DOMContentLoaded', navigate);
 
 // Fonction qui affiche la page d'accueil
-async function showMain() {
+async function main() {
     try {
         const product = new Product();
         const response = await product.readAll();
         const products = response.products;
 
-        let body = '';
-        for (let i = 0; i < products.length; i++) {
-            // prettier-ignore
-            body += `
-                <tr>
-                    <td>${products[i].name}</td>
-                    <td>${products[i].price / 100} €</td>
-                    <td>${products[i].quantity}</td>
-                    <td>
-                        <a href="#/update/${products[i]._id}" id="update-${i + 1}" class="btn btn-warning update" data-id="${products[i]._id}">
-                            Modifier
-                        </a>
-                        <button id="delete-${i + 1}" class="btn btn-danger delete" data-id="${products[i]._id}">
-                            Supprimer
-                        </button>
-                    </td>
-                </tr>
-            `;
-        }
-
-        container.innerHTML = `
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>Nom</th>
-                        <th>Prix</th>
-                        <th>Quantité</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    ${body}
-                    <tr>
-                        <td colspan="4"><a href="#/create" class="btn btn-primary w-100">Ajouter un produit</a></td>
-                    </tr>
-                </tbody
-            </table>
-        `;
+        showMain(products);
 
         const btnDelete = document.getElementsByClassName('delete');
         for (let i = 0; i < btnDelete.length; i++) {
@@ -92,25 +54,8 @@ async function showMain() {
 }
 
 // Fonction qui affiche le formulaire d'ajout
-function showCreate() {
-    container.innerHTML = `
-        <a href="#/" class="btn btn-primary mb-3">Accueil</a>
-        <form>
-            <div class="mb-3">
-                <label for="name" class="form-label">Nom</label>
-                <input type="text" class="form-control" id="name">
-            </div>
-            <div class="mb-3">
-                <label for="price" class="form-label">Prix (€)</label>
-                <input type="number" class="form-control" id="price">
-            </div>
-            <div class="mb-3">
-                <label for="quantity" class="form-label">Quantité</label>
-                <input type="number" class="form-control" id="quantity">
-            </div>
-            <button id="btn-create" class="btn btn-primary">Valider</button>
-        </form>
-    `;
+function create() {
+    showCreate();
 
     const btnCreate = document.getElementById('btn-create');
     btnCreate.addEventListener('click', async (e) => {
@@ -137,7 +82,7 @@ function showCreate() {
 }
 
 // Fonction qui affiche le formulaire de modification
-async function showUpdate(id) {
+async function update(id) {
     try {
         const product = new Product();
         product.setId(id);
@@ -146,25 +91,7 @@ async function showUpdate(id) {
         product.setPrice(response.product.price);
         product.setQuantity(response.product.quantity);
 
-        // prettier-ignore
-        container.innerHTML = `
-            <a href="#/" class="btn btn-primary mb-3">Accueil</a>
-            <form>
-                <div class="mb-3">
-                    <label for="name" class="form-label">Nom</label>
-                    <input type="text" class="form-control" id="name" value="${product.getName()}">
-                </div>
-                <div class="mb-3">
-                    <label for="price" class="form-label">Prix (€)</label>
-                    <input type="number" class="form-control" id="price" value="${product.getPrice() / 100}">
-                </div>
-                <div class="mb-3">
-                    <label for="quantity" class="form-label">Quantité</label>
-                    <input type="number" class="form-control" id="quantity" value="${product.getQuantity()}">
-                </div>
-                <button id="btn-update" class="btn btn-primary">Valider</button>
-            </form>
-        `;
+        showUpdate(product);
 
         const btnUpdate = document.getElementById('btn-update');
         btnUpdate.addEventListener('click', async (e) => {
---
+++ client/js/views/product.js
@@ -0,0 +1,93 @@
+export function showMain(products) {
+    const container = document.getElementById('container');
+
+    container.innerHTML = `
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Nom</th>
+                    <th>Prix</th>
+                    <th>Quantité</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody id="tbody">
+            </tbody
+        </table>
+    `;
+
+    let body = '';
+    for (let i = 0; i < products.length; i++) {
+        // prettier-ignore
+        body += `
+            <tr>
+                <td>${products[i].name}</td>
+                <td>${products[i].price / 100} €</td>
+                <td>${products[i].quantity}</td>
+                <td>
+                    <a href="#/update/${products[i]._id}" id="update-${i + 1}" class="btn btn-warning update" data-id="${products[i]._id}">
+                        Modifier
+                    </a>
+                    <button id="delete-${i + 1}" class="btn btn-danger delete" data-id="${products[i]._id}">
+                        Supprimer
+                    </button>
+                </td>
+            </tr>
+        `;
+    }
+    body += `
+        <tr>
+            <td colspan="4"><a href="#/create" class="btn btn-primary w-100">Ajouter un produit</a></td>
+        </tr>
+    `;
+
+    const tbody = document.getElementById('tbody');
+    tbody.innerHTML = body;
+}
+
+export function showCreate() {
+    const container = document.getElementById('container');
+
+    container.innerHTML = `
+        <a href="#/" class="btn btn-primary mb-3">Accueil</a>
+        <form>
+            <div class="mb-3">
+                <label for="name" class="form-label">Nom</label>
+                <input type="text" class="form-control" id="name">
+            </div>
+            <div class="mb-3">
+                <label for="price" class="form-label">Prix (€)</label>
+                <input type="number" class="form-control" id="price">
+            </div>
+            <div class="mb-3">
+                <label for="quantity" class="form-label">Quantité</label>
+                <input type="number" class="form-control" id="quantity">
+            </div>
+            <button id="btn-create" class="btn btn-primary">Valider</button>
+        </form>
+    `;
+}
+
+export function showUpdate(product) {
+    const container = document.getElementById('container');
+
+    // prettier-ignore
+    container.innerHTML = `
+        <a href="#/" class="btn btn-primary mb-3">Accueil</a>
+        <form>
+            <div class="mb-3">
+                <label for="name" class="form-label">Nom</label>
+                <input type="text" class="form-control" id="name" value="${product.getName()}">
+            </div>
+            <div class="mb-3">
+                <label for="price" class="form-label">Prix (€)</label>
+                <input type="number" class="form-control" id="price" value="${product.getPrice() / 100}">
+            </div>
+            <div class="mb-3">
+                <label for="quantity" class="form-label">Quantité</label>
+                <input type="number" class="form-control" id="quantity" value="${product.getQuantity()}">
+            </div>
+            <button id="btn-update" class="btn btn-primary">Valider</button>
+        </form>
+    `;
+}
